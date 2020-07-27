e11a25f19406cd66f79ee5551948970e03dcf187
Web Source (SRC) <61905565+web-sys1@users.noreply.github.com>
2020-07-27 08:02:56 +0300

    Update build_data.sh

--- bin/build_data.sh
+++ bin/build_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd $(dirname $0) /..
-cd covid19 & mkdir -p data-sources
+cd $(dirname $0) /../covid19
+mkdir -p data-sources
 
-sh ./bin/update_sources.sh
+sh update_sources.sh
 
 # Version data
 if git diff data/*.csv | grep . > /dev/null; then
@@ -11,7 +11,7 @@ if git diff data/*.csv | grep . > /dev/null; then
   git commit -m "update source data" covid-19/data-sources/*.csv
 
   # Build agregated data for dashboard
- python ./bin/export-main-countries.py
+ python export-main-countries.py
   git commit -m "update data" data/coronavirus-countries.json
 
   git push
